959cfb5a3fc52079eb2c3f350261c0de7699e286
Thandanani Ngcobo <Thandanani@explore-datascience.net>
2019-03-19 17:04:01 +0200

    commit

--- mypackage/sorting.py
+++ mypackage/sorting.py
@@ -10,7 +10,24 @@ def bubble_sort(items):
     '''Return array of items, sorted in ascending order'''
 
 
-def merge_sort(items):
+def merge(A, B):
+    new_list = []
+    while len(A) > 0 and len(B) > 0:
+        if A[0] < B[0]:
+            new_list.append(A[0])
+            A.pop(0)
+        else:
+            new_list.append(B[0])
+            B.pop(0)
+
+    if len(A) == 0:
+        new_list = new_list + B
+    if len(B) == 0:
+        new_list = new_list + A
+
+    return new_list
+
+    def merge_sort(items):
     len_i = len(items)
     if len_i == 1:
         return items
@@ -28,7 +45,7 @@ def quick_sort(items, index=-1):
     len_i = len(items)
 
     if len_i <= 1:
-        
+
         return items
 
     pivot = items[index]
